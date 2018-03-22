b152a945450b948f467ab947e60e502cd12ea21c
Benjie Kibblewhite <kibben@gmail.com>
2018-03-22 22:25:43 +0000

    layout for categories page

--- category.php
+++ category.php
@@ -1,28 +1,32 @@
 <?php get_header(); ?>
-
+	<!-- hero -->
+	<section class="hero">
+		<h1 class="heading heading-secondary"><?php single_cat_title(); ?></h1>	
+	</section>
 	<main role="main">
 		<!-- section -->
 		<section>
-			<h1><?php single_cat_title(); ?></h1>
 			<div class="category--grid">
 				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<!-- post title -->
-					<h2>
-						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-					</h2>
-					<!-- /post title -->
+				<article id="post-<?php the_ID(); ?>" <?php post_class('category--item'); ?>>
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 					<!-- featured image -->
-					<div class="category--featured-img">
-					<img src="<?php 
-						$featured_main = get_field( 'featured_main' );
-						if ( $featured_main ) :
-							echo $featured_main['url'];
-						elseif (has_post_thumbnail()) :
-							echo the_post_thumbnail_url();
-						endif;
-					?>">
+					<div class="category--item__img">
+						<img src="<?php 
+							$featured_main = get_field( 'featured_main' );
+							if ( $featured_main ) :
+								echo $featured_main['url'];
+							elseif (has_post_thumbnail()) :
+								echo the_post_thumbnail_url();
+							endif;
+						?>">
 					</div>
+					<!-- post title -->
+					<h2 class="heading category--item__title">
+						<?php the_title(); ?>
+					</h2>
+					</a>
+					<!-- /post title -->
 				</article>
 				<?php endwhile; ?>
 				<?php else: ?>
